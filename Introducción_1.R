061b53b6e2b035c8d796eb87cc634b33b35b43c2
esantiago24 <54640210+esantiago24@users.noreply.github.com>
2020-03-24 12:29:48 -0600

    Update Introducción_1.R

--- Introducción_1.R
+++ Introducción_1.R
@@ -21,15 +21,26 @@ qcstats <-
 filtered <-
     scater::quickPerCellQC(qcstats, percent_subsets = "subsets_Mito_percent")
 sce <- sce[, !filtered$discard]
+
 # Normalization.
 sce <- scater::logNormCounts(sce)
+
 # Feature selection.
 dec <- scran::modelGeneVar(sce)
 hvg <- scran::getTopHVGs(dec, prop = 0.1)
+
 # Dimensionality reduction.
 set.seed(1234)
 sce <- scater::runPCA(sce, ncomponents = 25, subset_row = hvg)
 sce <- scater::runUMAP(sce, dimred = 'PCA', external_neighbors = TRUE)
+
 # Clustering.
 g <- scran::buildSNNGraph(sce, use.dimred = 'PCA')
 sce$clusters <- factor(igraph::cluster_louvain(g)$membership)
+
+# Visualization.
+scater::plotUMAP(sce, colour_by = "clusters")
+
+## # Interactive visualization
+p <- scater::plotUMAP(sce, colour_by = "clusters")
+plotly::ggplotly(p)
