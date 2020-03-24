fec2ce782c012f11db9fe4fd7fa500abefbf0482
esantiago24 <54640210+esantiago24@users.noreply.github.com>
2020-03-24 12:20:39 -0600

    Create Introducción_1.R

---
+++ Introducción_1.R
@@ -0,0 +1,35 @@
+-------------------------------------------------------------------------
+library('scRNAseq')
+library('scater')
+library('scran')
+library('plotly')
+
+--------------------------------------------------------------------------------
+#Load object
+sce <- scRNAseq::MacoskoRetinaData()
+
+## How big is the data?
+pryr::object_size(sce)
+
+## How does it look?
+sce
+
+# Quality control.
+is.mito <- grepl("^MT-", rownames(sce))
+qcstats <-
+    scater::perCellQCMetrics(sce, subsets = list(Mito = is.mito))
+filtered <-
+    scater::quickPerCellQC(qcstats, percent_subsets = "subsets_Mito_percent")
+sce <- sce[, !filtered$discard]
+# Normalization.
+sce <- scater::logNormCounts(sce)
+# Feature selection.
+dec <- scran::modelGeneVar(sce)
+hvg <- scran::getTopHVGs(dec, prop = 0.1)
+# Dimensionality reduction.
+set.seed(1234)
+sce <- scater::runPCA(sce, ncomponents = 25, subset_row = hvg)
+sce <- scater::runUMAP(sce, dimred = 'PCA', external_neighbors = TRUE)
+# Clustering.
+g <- scran::buildSNNGraph(sce, use.dimred = 'PCA')
+sce$clusters <- factor(igraph::cluster_louvain(g)$membership)
